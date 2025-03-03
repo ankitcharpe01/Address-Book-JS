b57b1f86e52a14af197ab97b518eacf1e0936505
ankitcharpe01 <ankitcharpe01@gmail.com>
2025-03-03 16:38:03 +0530

    [Ankit Charpe]Add. UC8: Added  search person by city or state functionality

--- src/addressBokk.js
+++ src/addressBokk.js
@@ -65,3 +65,7 @@ class Contact {
       console.error('Duplicate Contact');
     }
   }
+
+  function searchByCityOrState(cityOrState) {
+    return addressBook.filter(contact => contact.city === cityOrState || contact.state === cityOrState);
+  }
