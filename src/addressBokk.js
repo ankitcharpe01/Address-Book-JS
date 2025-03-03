ca7fd71b597b9d7b36791dd3c5deb8759f4ffe8f
ankitcharpe01 <ankitcharpe01@gmail.com>
2025-03-03 16:19:44 +0530

    [Ankit Charpe]Add. UC4: Added edit contact functionality

--- src/addressBokk.js
+++ src/addressBokk.js
@@ -34,3 +34,10 @@ class Contact {
       console.error('Invalid Contact');
     }
   }
+
+  function editContactByName(name, newContact) {
+    const index = addressBook.findIndex(contact => contact.firstName === name.split(' ')[0] && contact.lastName === name.split(' ')[1]);
+    if (index !== -1) {
+      addressBook[index] = newContact;
+    }
+  }
